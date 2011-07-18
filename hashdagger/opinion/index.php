ed38c1d598cff2fa1600d0c0c20c3567477ca697
unknown <USER@.(none)>
2011-07-18 13:46:02 +0200

    refactoring Person::getData()

--- hashdagger/opinion/index.php
+++ hashdagger/opinion/index.php
@@ -7,18 +7,6 @@
 print_r($_POST);
 
 	$person = new Person;
-	$person->name  = $_POST['person-name'];
-	$person->age   = $_POST['person-age'];
-	$person->phone = $_POST['person-phone'];
-	$person->mail  = $_POST['person-mail'];
-		
-	if ($_POST['will-vote']) $person->will_vote = date("c");
-	if ($_POST['for-party']) $person->for_party = date("c");
-	if ($_POST['for-independent']) $person->for_independent = date("c");
-	$person->opinion = $_POST['reason'];
-	if ($_POST['supporter']) $person->is_supporter = date("c");
-	if ($_POST['volunteer']) $person->is_volunteer = date("c");
-	$person->note = $_POST['note'];
-	
+	$person->getData($_POST);	
 	$person->insert();
 
--- lib/person.php
+++ lib/person.php
@@ -16,5 +16,21 @@ class Person
 	{
 		return self::$table->insert($this);
 	}
+	
+	function getData($data)
+	{
+		$this->name  = $data['person-name'];
+		$this->age   = $data['person-age'];
+		$this->phone = $data['person-phone'];
+		$this->mail  = $data['person-mail'];
+			
+		if ($data['will-vote']) $this->will_vote = date("c");
+		if ($data['for-party']) $this->for_party = date("c");
+		if ($data['for-independent']) $this->for_independent = date("c");
+		$this->opinion = $data['reason'];
+		if ($data['supporter']) $this->is_supporter = date("c");
+		if ($data['volunteer']) $this->is_volunteer = date("c");
+		$this->note = $data['note'];
+	}
 
 } Person::$table = new ActiveRecord("person", "Person");
