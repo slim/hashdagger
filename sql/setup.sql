1b3b82f3bbad55eefbf19321a9920200cd0ef885
unknown <USER@.(none)>
2011-07-18 13:37:03 +0200

    new service "/opinion" new form "/enter/opinion"

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,58 @@
-create table canvass (id varchar(50) primary key, user_id varchar(50), volunteer_id varchar(50), person_id varchar(50), place_id varchar(50), begin datetime, end datetime, opened_door datetime, answered_questions datetime);
-create table place (id varchar(50) primary key, region varchar(50), district varchar(50), street varchar(50), building varchar(5), level varchar(5), house varchar(5));
-create table person (id varchar(50) primary key, name varchar(50), age int, phone varchar(50), mail varchar(50), place_id varchar(50));
-create table opinion (id varchar(50) primary key, person_id varchar(50), will_vote datetime, for_party datetime, for_independent datetime, reason varchar(150), is_supporter datetime, is_volunteer datetime, note varchar(150));
+-- --------------------------------------------------------
+
+-- 
+-- Structure de la table `canvass`
+-- 
+
+CREATE TABLE `canvass` (
+  `id` varchar(50) NOT NULL,
+  `user_id` varchar(50) default NULL,
+  `volunteer_id` varchar(50) default NULL,
+  `person_id` varchar(50) default NULL,
+  `place_id` varchar(50) default NULL,
+  `begin` datetime default NULL,
+  `end` datetime default NULL,
+  `opened_door` datetime default NULL,
+  `answered_questions` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB;
+
+-- --------------------------------------------------------
+
+-- 
+-- Structure de la table `person`
+-- 
+
+CREATE TABLE `person` (
+  `id` varchar(50) NOT NULL,
+  `name` varchar(50) default NULL,
+  `age` int(11) default NULL,
+  `phone` varchar(50) default NULL,
+  `mail` varchar(50) default NULL,
+  `place_id` varchar(50) default NULL,
+  `will_vote` datetime default NULL,
+  `for_party` datetime default NULL,
+  `for_independent` datetime default NULL,
+  `opinion` varchar(150) default NULL,
+  `is_supporter` datetime default NULL,
+  `is_volunteer` datetime default NULL,
+  `note` varchar(150) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB;
+
+-- --------------------------------------------------------
+
+-- 
+-- Structure de la table `place`
+-- 
+
+CREATE TABLE `place` (
+  `id` varchar(50) NOT NULL,
+  `region` varchar(50) default NULL,
+  `district` varchar(50) default NULL,
+  `street` varchar(50) default NULL,
+  `building` varchar(5) default NULL,
+  `level` varchar(5) default NULL,
+  `house` varchar(5) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB;
