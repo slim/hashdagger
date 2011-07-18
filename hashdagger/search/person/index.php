77e6a21a7d05e2ea7d0d66de939aeed955ab742d
anis chaabani <anis@platysystems.com>
2011-07-18 17:27:12 +0200

    service "/search/person"

---
+++ hashdagger/search/person/index.php
@@ -0,0 +1,55 @@
+<?php
+	require "../../../ini.php";
+	require "../../../lib/activerecord.php";
+	require "../../../lib/person.php";
+	ActiveRecord::$db = $ini['DB'];
+	Person::$db = $ini['DB'];
+
+	$persons = Person::selectByUser($_GET['user_id']);
+
+	?>
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<!-- service -->
+	<head>
+		<title>Canvass - HashDagger</title> 
+		<link rel="stylesheet" type="text/css" href="../../css/barred.css" />
+		<link rel="stylesheet" type="text/css" href="../../css/wforms.css" />
+	</head>
+	<body dir="rtl">
+	<table>
+	<thead>
+	<tr>
+		<th>السم و اللقب</th>
+		<th>العمر</th>
+		<th>الهاتف</th>
+		<th>البريد الالكتروني</th>
+		<th>انتخاب</th>
+		<th>الاحزاب</th>
+		<th>القوائم المستقلة</th>
+		<th>لماذا؟</th>
+		<th>مساند</th>
+		<th>متطوع</th>
+		<th>ملاحظة</th>
+	</tr>
+	</thead>
+	<tbody>
+	<?php
+	foreach($persons AS $person)
+	{
+		echo "<tr>";
+		echo "<td>".$person->name."</td>";
+		echo "<td>".$person->age."</td>";
+		echo "<td>".$person->phone."</td>";
+		echo "<td>".$person->mail."</td>";
+		echo "<td>".$person->will_vote."</td>";
+		echo "<td>".$person->for_party."</td>";
+		echo "<td>".$person->for_independent."</td>";
+		echo "<td>".$person->opinion."</td>";
+		echo "<td>".$person->is_supporter."</td>";
+		echo "<td>".$person->is_volunteer."</td>";
+		echo "<td>".$person->note."</td>";
+		echo "</tr>";
+	}
+?>
+	</tbody>
+	</table>
--- lib/person.php
+++ lib/person.php
@@ -1,11 +1,12 @@
 <?php
 
-	require_once "activerecord.php";
+require_once "activerecord.php";
 
 class Person
 {
 	static $table;
 	public $id;
+	static $db;
 
 	function __construct()
 	{
@@ -31,6 +32,33 @@ class Person
 		if ($data['supporter']) $this->is_supporter = date("c");
 		if ($data['volunteer']) $this->is_volunteer = date("c");
 		$this->note = $data['note'];
+	}
+	
+	static function selectByUser($user_id)
+	{
+		$persons = Array();
+		$query = self::$db->prepare('select * from person where user_id=:user_id');
+		$query->bindValue(':user_id', $user_id);		
+      	$result = $query->execute();
+      	
+		foreach ($query->fetchAll() as $entry) {
+	   		$person = new Person();   		
+			$person->id = $entry['id'];
+			$person->name = $entry['name'];
+			$person->age = $entry['age'];
+			$person->phone = $entry['phone'];
+			$person->mail = $entry['mail'];
+			$person->will_vote = $entry['will_vote'];
+			$person->for_party = $entry['for_party'];
+			$person->for_independent = $entry['for_independent'];
+			$person->opinion = $entry['opinion'];
+			$person->is_supporter = $entry['is_supporter'];
+			$person->is_volunteer = $entry['is_volunteer'];
+			$person->note = $entry['note'];
+	   		$persons [] = $person;   
+	    }
+	    
+	    return $persons;
 	}
 
 } Person::$table = new ActiveRecord("person", "Person");
