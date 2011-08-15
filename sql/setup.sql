25649c87f2070e1a9dde236ee36465913eece62c
anis chaabani <anis@platysystems.com>
2011-08-15 14:41:10 +0200

    test unique mail and phone

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,52 +1,50 @@
--- 
--- Structure de la table `canvass`
--- 
-
-CREATE TABLE `canvass` (
+CREATE TABLE IF NOT EXISTS `canvass` (
   `id` varchar(50) NOT NULL,
-  `user_id` varchar(50) default NULL,
-  `volunteer_id` varchar(50) default NULL,
-  `person_id` varchar(50) default NULL,
-  `place_id` varchar(50) default NULL,
-  `begin` datetime default NULL,
-  `end` datetime default NULL,
-  `opened_door` datetime default NULL,
-  `answered_questions` datetime default NULL,
-  `address_region` varchar(50) default NULL,
-  `address_district` varchar(50) default NULL,
-  `address_street` varchar(50) default NULL,
-  `address_building` varchar(5) default NULL,
-  `address_level` varchar(5) default NULL,
-  `address_house` varchar(5) default NULL,
-  PRIMARY KEY  (`id`)
+  `user_id` varchar(50) DEFAULT NULL,
+  `volunteer_id` varchar(50) DEFAULT NULL,
+  `person_id` varchar(50) DEFAULT NULL,
+  `place_id` varchar(50) DEFAULT NULL,
+  `begin` datetime DEFAULT NULL,
+  `end` datetime DEFAULT NULL,
+  `opened_door` datetime DEFAULT NULL,
+  `answered_questions` datetime DEFAULT NULL,
+  `address_region` varchar(50) DEFAULT NULL,
+  `address_district` varchar(50) DEFAULT NULL,
+  `address_street` varchar(50) DEFAULT NULL,
+  `address_building` varchar(5) DEFAULT NULL,
+  `address_level` varchar(5) DEFAULT NULL,
+  `address_house` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Structure de la table `person`
--- 
+--
 
-CREATE TABLE `person` (
+CREATE TABLE IF NOT EXISTS `person` (
   `id` varchar(50) NOT NULL,
   `user_id` varchar(50) NOT NULL,
   `login` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `is_user` datetime default NULL,
-  `name` varchar(50) default NULL,
-  `age` int(11) default NULL,
-  `phone` varchar(50) default NULL,
-  `mail` varchar(50) default NULL,
-  `will_vote` datetime default NULL,
-  `for_party` datetime default NULL,
-  `for_independent` datetime default NULL,
-  `opinion` varchar(150) default NULL,
-  `is_supporter` datetime default NULL,
-  `is_volunteer` datetime default NULL,
-  `note` varchar(150) default NULL,
+  `is_user` datetime DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `phone_signature` varchar(50) NOT NULL,
+  `mail` varchar(50) DEFAULT NULL,
+  `mail_signature` varchar(50) NOT NULL,
+  `will_vote` datetime DEFAULT NULL,
+  `for_party` datetime DEFAULT NULL,
+  `for_independent` datetime DEFAULT NULL,
+  `opinion` varchar(150) DEFAULT NULL,
+  `is_supporter` datetime DEFAULT NULL,
+  `is_volunteer` datetime DEFAULT NULL,
+  `note` varchar(150) DEFAULT NULL,
   `user_key` varchar(50) NOT NULL,
   `creator_key` varchar(50) NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY (`id`)
 );
 
 insert into person (id, name, login, password, is_user, user_key) values (1, "First User", "h", md5('mosta9ella'), now(), aes_encrypt('mefta7', 'mosta9ella'));
