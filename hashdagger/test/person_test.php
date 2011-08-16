76369bc8e8caafe9ac905b6335f379b7c473493a
Slim Amamou <slim.amamou@gmail.com>
2011-08-16 09:44:30 +0100

    test: Person::selectById() decrypts personal info

--- hashdagger/test/person_test.php
+++ hashdagger/test/person_test.php
@@ -3,30 +3,39 @@
 	require_once "../../lib/person.php";
 	require_once "../../lib/user.php";
 
+	$USER = new User;
+	$USER->user_key = "1234567890123456";
+	$USER->password = "6543210987654321";
+	$PERSON = new Person;
+	$PERSON->name = "Slim Amamou";
+	$PERSON->mail = "slim.amamou@gmail.com";
+	$PERSON->user_id= $USER->id;
+
 class TestOfPerson extends UnitTestCase {
 	function setUp()
 	{
 		$this->db = new PDO("mysql:host=localhost;dbname=hashdagger_test", "root");
 		$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 		Person::$db = $this->db;
-		$this->person = new Person;
-		$this->person->name = "Slim Amamou";
-		$this->person->mail = "slim.amamou@gmail.com";
 	}
 
 	function testInsertEncryptsPersonalInfo()
 	{
-		global $USER;
+		global $USER, $PERSON;
 
-		$USER = new User;
-		$USER->user_key = "1234567890123456";
-		$USER->password = "6543210987654321";
-		$this->person->user_id= $USER->id;
-		$this->person->insert();
-	    $result = $this->db->query("select * from person where id='".$this->person->id."'");
-		$person = $result->fetch();
-		$this->assertEqual(md5($person['name']), "e43275f823f1e9064fa6d4a31def8a07"); //"Slim Amamou" AES encrypted with "1234567890123456"
+		$PERSON->insert();
+	    $result = $this->db->query("select * from person where id='".$PERSON->id."'");
+		$p = $result->fetch();
+		$this->assertEqual(md5($p['name']), "e43275f823f1e9064fa6d4a31def8a07"); //"Slim Amamou" AES encrypted with "1234567890123456"
 
+	}
 
+	function testSelectDecryptsPersonalInfo()
+	{
+		global $USER, $PERSON;
+
+		$p = Person::selectById($PERSON->id);
+
+		$this->assertEqual($p->name, "Slim Amamou");
 	}
 }
