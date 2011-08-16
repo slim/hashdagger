64b6ae913c24fda6d9efe726bdc1369c6ec3b0a6
Slim Amamou <slim.amamou@gmail.com>
2011-08-16 11:32:09 +0100

    test: Person::becomUser() and Person::update()

--- hashdagger/test/person_test.php
+++ hashdagger/test/person_test.php
@@ -35,7 +35,42 @@ class TestOfPerson extends UnitTestCase {
 		global $USER, $PERSON;
 
 		$p = Person::selectById($PERSON->id);
-
 		$this->assertEqual($p->name, "Slim Amamou");
+
+		list($p) = Person::selectByUser($PERSON->user_id);
+		$this->assertEqual($p->name, "Slim Amamou");
+
 	}
+
+	function testUpdate()
+	{
+		global $USER, $PERSON;
+
+		$PERSON->name = "Anis Chaabani";
+		$PERSON->update();
+	    $result = $this->db->query("select * from person where id='".$PERSON->id."'");
+		$p = $result->fetch();
+		$this->assertEqual(md5($p['name']), "b8a0d847725567ef4886a5e7f584f726"); //"Anis Chaabani" AES encrypted with "1234567890123456"
+
+	}
+
+	function testBecomeUser()
+	{
+		global $USER, $PERSON;
+
+		$PERSON->becomeUser();
+		$creator_password = $USER->password;
+		$person_password = $PERSON->password;
+		$person_id = $PERSON->id;
+	    $result = $this->db->query("select is_user, login, aes_decrypt(user_key, '$person_password') as user_key, aes_decrypt(creator_key, '$creator_password') as creator_key from person where id='$person_id'");
+		$p = $result->fetch();
+		$this->assertNotNull($p['is_user']); 
+		$this->assertEqual($p['login'], "slim.amamou@gmail.com"); 
+		$this->assertEqual($p['creator_key'], $p['user_key']);
+		$key = $p['user_key'];
+	    $result = $this->db->query("select aes_decrypt(name, '$key') as name from person where id='$person_id'");
+		$p = $result->fetch();
+		$this->assertEqual($p['name'], "Anis Chaabani"); 
+	}
+
 }
